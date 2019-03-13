ef01677e71cfda6d2674dbf93a4775b436320d4f
perlancar (on pc-office) <perlancar@gmail.com>
2019-03-13 20:33:05 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/GoodInterfaces.pm
@@ -0,0 +1,25 @@
+package Acme::CPANModules::GoodInterfaces;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => 'Modules with good interfaces',
+    description => <<'_',
+
+This is my subjective list of modules that I think have particularly nice
+interfaces (APIs) and are convenient to use. Modules that are a delight to use
+(in Marie Kondo lingo: "spark joy") and remind you why you love Perl and
+programming in general.
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 append:SEE ALSO
+
+L<Acme::CPANModules::BadInterfaces>
